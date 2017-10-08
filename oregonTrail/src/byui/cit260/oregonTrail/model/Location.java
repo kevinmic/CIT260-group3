ef0601850983c4d6e7f379e76b5d720a0e2053ab
James <James@LAPTOP-ED6RTQ7A>
2017-10-07 21:43:31 -0700

    testing a commit

--- oregonTrail/src/byui/cit260/oregonTrail/model/Location.java
+++ oregonTrail/src/byui/cit260/oregonTrail/model/Location.java
@@ -4,12 +4,29 @@
  * and open the template in the editor.
  */
 package byui.cit260.oregonTrail.model;
-
-
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Dresen_HP
  */
+class Location implements Serializable{
+// instance variables
+private int currentMiles;
+private string locationVisited;
+private int milesRemaining;
+private String type;
+private int column;
+private String visited;
+private int amountremaining;
+private String type2;
+public String weather;
+
+// relationships with other classes
+public List<Location> location = new ArrayList<>(); //0-*
+// default constructor
 class Location {
     
 }
+
