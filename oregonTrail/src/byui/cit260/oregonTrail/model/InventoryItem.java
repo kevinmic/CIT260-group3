9f3c677730b6188ff0f48e02eeab3efcbb258c03
jordan <jordan@192.168.1.6>
2017-10-07 17:14:08 -0700

    Finished getter and setter, constructor, equals() and hashCode(), and toString()
    All done!

--- oregonTrail/src/byui/cit260/oregonTrail/model/InventoryItem.java
+++ oregonTrail/src/byui/cit260/oregonTrail/model/InventoryItem.java
@@ -20,9 +20,113 @@ public class InventoryItem implements Serializable {
     private double requiredAmount;
     private double basePrice;
     
-    // relationships with other classes
+    //relationships with other classes
     private List<Game> games = new ArrayList<>(); //0-*
+    private List<BarterScene> barterScene = new ArrayList<>(); //0-*
     
+    //default constructor
+
+    public InventoryItem() {
+    }
+    
+    //getter and setter methods
+    public String getInventoryType() {
+        return inventoryType;
+    }
+
+    public void setInventoryType(String inventoryType) {
+        this.inventoryType = inventoryType;
+    }
+
+    public double getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(double quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public double getRequiredAmount() {
+        return requiredAmount;
+    }
+
+    public void setRequiredAmount(double requiredAmount) {
+        this.requiredAmount = requiredAmount;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
+
+    public List<BarterScene> getBarterScene() {
+        return barterScene;
+    }
+
+    public void setBarterScene(List<BarterScene> barterScene) {
+        this.barterScene = barterScene;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.inventoryType);
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.quantityInStock) ^ (Double.doubleToLongBits(this.quantityInStock) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.requiredAmount) ^ (Double.doubleToLongBits(this.requiredAmount) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.basePrice) ^ (Double.doubleToLongBits(this.basePrice) >>> 32));
+        hash = 79 * hash + Objects.hashCode(this.games);
+        hash = 79 * hash + Objects.hashCode(this.barterScene);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InventoryItem other = (InventoryItem) obj;
+        if (Double.doubleToLongBits(this.quantityInStock) != Double.doubleToLongBits(other.quantityInStock)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.requiredAmount) != Double.doubleToLongBits(other.requiredAmount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.basePrice) != Double.doubleToLongBits(other.basePrice)) {
+            return false;
+        }
+        if (!Objects.equals(this.inventoryType, other.inventoryType)) {
+            return false;
+        }
+        if (!Objects.equals(this.games, other.games)) {
+            return false;
+        }
+        if (!Objects.equals(this.barterScene, other.barterScene)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem{" + "inventoryType=" + inventoryType + ", quantityInStock=" + quantityInStock + ", requiredAmount=" + requiredAmount + ", basePrice=" + basePrice + ", games=" + games + ", barterScene=" + barterScene + '}';
+    }
     
     
 }
