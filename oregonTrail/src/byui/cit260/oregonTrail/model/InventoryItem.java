6de2bf25326a55a14fbb0246c8d54e777dd9e9c1
jordan <jordan@192.168.1.6>
2017-10-07 17:07:52 -0700

    Adding variables

--- oregonTrail/src/byui/cit260/oregonTrail/model/InventoryItem.java
+++ oregonTrail/src/byui/cit260/oregonTrail/model/InventoryItem.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package byui.cit260.oregonTrail.model;
-
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.awt.Point;
 /**
  *
- * @author Dresen_HP
+ * @author jones-jordan
  */
-public class InventoryItem {
+public class InventoryItem implements Serializable {
+    //class instance variables
+    private String inventoryType;
+    private double quantityInStock;
+    private double requiredAmount;
+    private double basePrice;
+    
+    // relationships with other classes
+    private List<Game> games = new ArrayList<>(); //0-*
+    
+    
     
 }
