639ce59295abe3edae5d241c9c7f431c65112df1
Dresen_HP <Dresen_HP@hp-glacier>
2017-10-17 13:28:38 -0700

    last fix

--- oregonTrail/src/byui/cit260/oregonTrail/model/Game.java
+++ oregonTrail/src/byui/cit260/oregonTrail/model/Game.java
@@ -5,12 +5,9 @@
  */
 package byui.cit260.oregonTrail.model;
 import java.io.Serializable;
-<<<<<<< HEAD
-=======
 import java.util.ArrayList;
 import java.util.List;
 
->>>>>>> b9cf1f0b8929aeba0aeb371b363d6825b19fc87d
 /**
  *
  * @author Dresen_HP
@@ -22,16 +19,11 @@ public class Game implements Serializable{
     private double percentComplete;
     private int highScores;
     private int noPlayers;
-<<<<<<< HEAD
-    private Player player;
-
-=======
     // relationships with other classes
     private Player player; // 1 to 1
     private Map map; // 1 to 1
     private List<Actor> actors = new ArrayList<>(); // 0-*
     private List<InventoryItem> inventoryItems = new ArrayList<>(); // 0-*
->>>>>>> b9cf1f0b8929aeba0aeb371b363d6825b19fc87d
 
     
    // constructor
@@ -87,8 +79,6 @@ public class Game implements Serializable{
     public void setPlayer(Player player) {
         this.player = player;
     }
-<<<<<<< HEAD
-=======
     
     public Map getMap() {
         return map;
@@ -114,7 +104,6 @@ public class Game implements Serializable{
         this.inventoryItems = inventoryItems;
     }
     
->>>>>>> b9cf1f0b8929aeba0aeb371b363d6825b19fc87d
 
     @Override
     public int hashCode() {
@@ -165,13 +154,5 @@ public class Game implements Serializable{
     
     
     
-<<<<<<< HEAD
-}
-=======
 
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> b9cf1f0b8929aeba0aeb371b363d6825b19fc87d
->>>>>>> 02604f9a4855f1f0965c3993cb19f7af94879074
