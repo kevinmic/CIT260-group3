ace60e8ccaeef919fec77d31f25fc2d15fe9ec96
jones-jordan16046 <31944868+jones-jordan16046@users.noreply.github.com>
2017-10-25 19:16:10 -0700

    Merge origin/master
    Conflicts:
    	oregonTrail/src/byui/cit260/oregonTrail/control/RiverControl.java
    	oregonTrail/src/byui/cit260/oregonTrail/model/Game.java
    	oregonTrail/src/byui/cit260/oregonTrail/model/Player.java
    	oregonTrail/test/byui/cit260/oregonTrail/control/RiverControlTest.java
    
    completed through page 35 of team assignment L06

--- oregonTrail/src/byui/cit260/oregonTrail/view/StartProgramView.java
+++ oregonTrail/src/byui/cit260/oregonTrail/view/StartProgramView.java
@@ -5,6 +5,10 @@
  */
 package byui.cit260.oregonTrail.view;
 
+import byui.cit260.oregonTrail.control.GameControl;
+import byui.cit260.oregonTrail.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author James
@@ -54,8 +58,76 @@ public class StartProgramView {
     }
 
     public void displayStartProgramView() {
-            System.out.println("\n*** displayStartProgram() function called ***");
+                              
+            boolean done = false; // set flag to not done
+            do {
+                //prompt for and get player's name
+                String playersName = this.getPlayersName();
+                if (playersName.toUpperCase().equals("Q")) // user wants to quit
+                    return; //exit the game
+                 
+                //do the requested action and display the next view
+                done = this.doAction(playersName);
+            } while (!done);
+    }
+
+    private String getPlayersName() {
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; //initialize to not valid
+        
+        while (!valid) { //loop while an invalid value is entered
+            System.out.println("\n" + this.promptMessage);
             
+            value = keyboard.nextLine(); //get next line typed on keyboard
+            value = value.trim(); //trim off leading and trailing blanks
+            
+            if (value.length() < 1) { //value is blank
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+            }
+            
+            break; //end the loop
+        }
+        
+        return value; //return the value entered
+    }
+
+    private boolean doAction(String playersName) {
+        if (playersName.length() < 2) {
+            System.out.println("\nInvalid players name: "
+            + "The name must be greater than one character in length");
+            return false;
+        }
+        
+        // call createPlayer() control function
+        Player player = GameControl.createPlayer(playersName);
+        
+        if (player == null) { //if unsuccessful
+            System.out.println("\nError creating the player.");
+            return false;
+        }
+        
+        //display next view
+        this.displayNextView(player);
+        
+        return true; //success!
+    }
+
+    private void displayNextView(Player player) {
+        //dispaly a custom welcome message
+        System.out.println("\n==========================================="
+                           +"\n Welcome to the game " + player.getName()
+                           +"\n We hope you have a lot of fun!"
+                           +"\n==========================================="
+                           );
+        
+        //create MainMenuView object
+        MainMenuView mainMenuView = new MainMenuView();
+                
+        //display the main menue view
+        mainMenuView.displayMainMenuView();
+        
     }
 }
 
