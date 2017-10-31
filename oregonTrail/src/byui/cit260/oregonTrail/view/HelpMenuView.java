f3358157805e44e35ef8fce579fbdb28187a9d56
Dresen_HP <Dresen_HP@hp-glacier>
2017-10-30 18:59:24 -0700

    Finished help view

--- oregonTrail/src/byui/cit260/oregonTrail/view/HelpMenuView.java
+++ oregonTrail/src/byui/cit260/oregonTrail/view/HelpMenuView.java
@@ -94,15 +94,81 @@ class HelpMenuView {
     }
 
     private void displayLearnOregonTrail() { // called from helpOption() in this class
-        System.out.println("\n*** displayLearnOregonTrail() stub function called ***");
+        System.out.println("\n**************************************************************"
+                            +"\n* The Oregon Trail                                            *"
+                            +"\n*-------------------------------------------------------------*"                                                            
+                            + "\n* This game takes place along the 1850’s era Oregon Trail.   *"
+                            + "\n* In this game, the player takes the place of one of several *" 
+                            + "\n* characters as he or she embarks along the trek west. Along *"
+                            + "\n* the way, the player is faced with several challenges and   *"
+                            + "\n* missions that he or she needs to compete in order to win   *"
+                            + "\n* the game.                                                  *"
+                            + "\n*                                                            *"
+                            + "\n* After starting a game, the user is prompted to select his  *"
+                            + "\n* or her character type (farmer, blacksmith, merchant).      *"
+                            + "\n* Each profession comes with it different starting amounts   *"
+                            + "\n* and ability to earn points. Then the traveler names each   *"
+                            + "\n* member of his or her party and purchases inventory to be   *"
+                            + "\n* used along the trail. After selecting the time of year to  *"
+                            + "\n* embark, the journey begins.                                *"
+                            + "\n*                                                            *"
+                            + "\n* Along the trail, the company is faced with many difficult  *"
+                            + "\n* decisions that could bring great rewards or exact great    *"
+                            + "\n* cost to the party. They will cross rivers, face sickness   *"
+                            + "\n* and famine, hunt for food, trade with natives or fellow    *"
+                            + "\n* travelers and interact with others at each checkpoint.     *"
+                            + "\n*                                                            *"
+                            + "\n* The game tests the mettle of even the most hardened        *"
+                            + "\n* adventure. Can you make it in the 1850’s travel west?      *"
+                            + "\n* Only time will tell.                                       *"
+                            + "\n*                                                            *"
+                            + "\n**************************************************************"
+                            + "\n"
+        );
+                            this.displayHelpMenuView();
     }
+    
 
     private void displayObjectives() { // called from helpOption() in this class
-        System.out.println("\n*** displayObjectives() stub function called ***");
+        System.out.println("\n**************************************************************"
+                            +"\n* Objectives Of the Game                                      *"
+                            +"\n*-------------------------------------------------------------*"
+                            + "\n*                                                            *"
+                            + "\n* Learn More about the history of the Oregon Trail           *"
+                            + "\n*                                                            *" 
+                            + "\n* Get your family safely to Oregon                           *"
+                            + "\n*    This is done making critical decisions about how        *"
+                            + "\n*    fast to travel, how many supplies to purchase,          *"
+                            + "\n*    whether to hunt, continue on the trail, hire guides,    *"
+                            + "\n*    barter, talk to people along the way, and whether to    *"
+                            + "\n*    take risks or play it safe.                             *"
+                            + "\n*                                                            *"
+                            + "\n* Have fun!                                                  *"
+                            + "\n*                                                            *"                               
+                            + "\n*                                                            *"
+                            + "\n**************************************************************"
+                            + "\n"
+        );
+        this.displayHelpMenuView();
     }
 
     private void displayOccupations() { // called from helpOption() in this class
-        System.out.println("\n*** displayOccupations() stub function called ***");
+                System.out.println("\n**************************************************************"
+                            +"\n* Occupation Choices                                          *"
+                            +"\n*-------------------------------------------------------------*"
+                            + "\n*                                                            *"
+                            + "\n* Blacksmith: Wagon will not break down on trail.            *"
+                            + "\n*                                                            *" 
+                            + "\n* Farmer: Points are doubled at the end of game.             *"
+                            + "\n*                                                            *"
+                            + "\n* Merchant: Starts game with an extra $200.                  *"
+                            + "\n* Have fun!                                                  *"
+                            + "\n*                                                            *"                               
+                            + "\n*                                                            *"
+                            + "\n**************************************************************"
+                            + "\n"
+                            );
+                this.displayHelpMenuView();
     }
 
 
